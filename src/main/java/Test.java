5c6af57f58d895985c808d4b24d1b5f2555e7dd9
cescoferraro <francescoaferraro@gmail.com>
2017-05-12 22:24:45 -0300

    SQL Connection working with docker image!

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,16 +1,20 @@
 package main.java;
 
 import java.util.Scanner;
+import java.sql.*; 
 
 public class Test {
-    public static void main(String... args) {
-	System.out.println("Eclim is awesome!");
+    public static void main(String... args) throws SQLException {
+		Connection conexao = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:49161/xe","system","oracle"); 
+		System.out.println(conexao);
+		conexao.close(); 
+		System.out.println("SQL is awesome!");
     }
 
     public static String AskString(){
-	Scanner entrada = new Scanner(System.in);
-	String result = entrada.next();
-	entrada.close();
-	return result; 
+		Scanner entrada = new Scanner(System.in);
+		String result = entrada.next();
+		entrada.close();
+		return result; 
     }
 }
