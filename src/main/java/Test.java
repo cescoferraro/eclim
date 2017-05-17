e0f9867b7daec6e593d72778657ee5e234cacbaa
cescoferraro <francescoaferraro@gmail.com>
2017-05-16 21:36:41 -0300

    2017-05-16 21:36

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String... args) {
-	System.out.println("Eclim is awesome!");
-    }
-
-    public static String AskString(){
-	Scanner entrada = new Scanner(System.in);
-	String result = entrada.next();
-	entrada.close();
-	return result; 
+		System.out.println("Eclim is awesome!");
     }
 }
