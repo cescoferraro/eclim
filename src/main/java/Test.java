e9e811ed4f135e68856481dbd4b92f8bd72239e7
cescoferraro <francescoaferraro@gmail.com>
2017-05-31 19:17:47 -0300

    burger lord initial

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String... args) {
-		System.out.println("Eclim is awesome!");
+		System.out.println("Burger Lord");
     }
 }
