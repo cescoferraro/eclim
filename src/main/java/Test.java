fb8f546006ff9d30a707620ff1cc124fed022319
cescoferraro <francescoaferraro@gmail.com>
2017-05-31 20:36:34 -0300

    2017-05-31 20:36

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,42 +1,19 @@
 package main.java;
 
+
 import java.util.Arrays;
+
 import main.library.Library;
 
 public class Test {
 	public static void main(String... args) {
 		Library lib = new Library();
 		// Math Numbers tests
-		System.out.println("// Math Numbers tests");
-		int perfectCount = 0;
-		int abundantCount = 0;
-		int deficientCount = 0;
-		for (int i = 1; i <= 100; i++) {
-			if (lib.isPerfect(i))
-				perfectCount++;
-			if (lib.isAbundant(i))
-				abundantCount++;
-			if (lib.isDeficient(i))
-				deficientCount++;
+
+		for (int clientes = 0; clientes < 1000; clientes++) {
+			System.out.println(lib.FilialRandom());
+			// System.out.println(lib.RandomLancheQuantidade());
+			System.out.println(Arrays.toString(lib.Pedido()));
 		}
-		System.out.println(perfectCount);
-		System.out.println(abundantCount);
-		System.out.println(deficientCount);
-		System.out.println((deficientCount + abundantCount + perfectCount) == 100);
-		// Multiple tests
-		System.out.println("// Multiple tests");
-		System.out.println(lib.isMultiple(2, 4));
-		System.out.println(lib.isMultiple(2, 3));
-		// Heads or tails tests
-		System.out.println("// Heads or tails tests");
-		System.out.println(lib.isHeads(3));
-		// Size tests
-		System.out.println("// Size tests");
-		System.out.println(lib.Smallest(2, 32, 9));
-		System.out.println(lib.Biggest(2, 32, 9));
-		// Sorting tests
-		System.out.println("// Sorting tests");
-		System.out.println(Arrays.toString(lib.SortUP(2, 32, 9)));
-		System.out.println(Arrays.toString(lib.SortDOWN(2, 32, 9)));
 	}
 }
--- src/main/library/Library.java
+++ src/main/library/Library.java
@@ -1,121 +1,39 @@
 package main.library;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Random;
 
 public class Library {
 	public static void main(String... args) {
 		System.out.println("Library is instanciated!");
 	}
 
-	// Math Number types
-	private int commonAlgorhythm(int number) {
-		int temp = 0;
-		for (int i = 1; i <= number / 2; i++) {
-			if (number % i == 0) {
-				temp += i;
+	public static int[][] VendasMatriz(){
+		int[][] vendas = new int[1000][4];
+		return vendas;
+	} 
+
+
+	public static int RandomRange(int min, int max) {
+		Random rand = new Random();
+		return min + rand.nextInt((max - min) + 1);
+	}
+
+	public static int[] Pedido() {
+		int[]  pedido = new int [4];
+		int max = 5;
+		for (int i = 0 ; i < pedido.length; i ++){
+			int qtde = RandomRange(1,max);
+			if ( max > 0  ){
+				pedido[i] = qtde;
+				max = max - qtde;
+			} else {
+				pedido[i]=  0;
 			}
 		}
-		return temp;
+		return pedido;  
 	}
 
-	public boolean isPerfect(int number) {
-		return commonAlgorhythm(number) == number;
+	public static int FilialRandom() {
+		return RandomRange(1, 3); 
 	}
-
-	public boolean isDeficient(int number) {
-		return commonAlgorhythm(number) < number;
-	}
-
-	public boolean isAbundant(int number) {
-		return commonAlgorhythm(number) > number;
-	}
-
-	// Extra methods
-	public boolean isHeads(int number) {
-		return number % 2 == 0;
-	}
-
-	public boolean isMultiple(int a, int b) {
-		return b % a == 0;
-	}
-
-	// Areas
-	// (quadrado, retângulo, circulo, triângulo)
-	public Double SquareArea(Double side) {
-		return Math.pow(side, 2);
-	}
-
-	public Double ParallelogramArea(Double sideA, Double sideB) {
-		return (sideA * sideB);
-	}
-
-	public Double TriangleArea(Double base, Double height) {
-		return (base * height) / 3;
-	}
-
-	public Double CircleArea(Double radius) {
-		return Math.pow(radius, 2) * Math.PI;
-	}
-
-	// Superfície Piramyd Missing
-	// paralelepípedo, pirâmide, cilindro, esfera
-	public Double ParallelepipedSurfaceArea(Double sideA, Double sideB, Double height) {
-		return (2 * (sideA * sideB) + 2 * (sideA * height) + 2 * (sideB + height));
-	}
-
-	public Double CilinderSurfaceArea(Double radius, Double height) {
-		return ((2 * radius * Math.PI) * height) + (2 * Math.PI + Math.pow(radius, 2));
-	}
-
-	public Double EsphereSurfaceArea(Double radius) {
-		return (4) * Math.pow(radius, 2) * Math.PI;
-	}
-
-	// Volumes DONE!
-	// : paralelepípedo, pirâmide, cilindro, esfera
-	public Double ParallelepipedVolume(Double sideA, Double sideB, Double sideC) {
-		return sideA * sideB * sideC;
-	}
-
-	public Double PyramidVolume(Double sideA, Double sideB, Double height) {
-		return (sideA * sideB) * height;
-	}
-
-	public Double CilinderVolume(Double radius, Double height) {
-		return (height) * Math.pow(radius, 2) * Math.PI;
-	}
-
-	public Double EsphereVolume(Double radius) {
-		return (4 / 3) * Math.pow(radius, 3) * Math.PI;
-	}
-
-	public int Smallest(int a, int b, int c) {
-		int[] tab = { a, b, c };
-		return Arrays.stream(tab).min().getAsInt();
-	}
-
-	public int Biggest(int a, int b, int c) {
-		int[] tab = { a, b, c };
-		return Arrays.stream(tab).max().getAsInt();
-	}
-
-	public int[] SortUP(int a, int b, int c) {
-		int[] tab = { a, b, c };
-		Arrays.sort(tab);
-		return tab;
-	}
-
-	Comparator<Integer> comparator = new Comparator<Integer>() {
-		@Override
-		public int compare(Integer o1, Integer o2) {
-			return o2.compareTo(o1);
-		}
-	};
-
-	public Integer[] SortDOWN(int a, int b, int c) {
-		Integer[] tab = { a, b, c };
-		Arrays.sort(tab, comparator);
-		return tab;
-	   }
 }
