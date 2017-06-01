682d6b2b1a3912a1e273b82e12240811127ffa7d
cescoferraro <francescoaferraro@gmail.com>
2017-05-31 21:01:28 -0300

    2017-05-31 21:01

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,11 +9,17 @@ public class Test {
 	public static void main(String... args) {
 		Library lib = new Library();
 		// Math Numbers tests
+		int [][] Matrix = lib.VendasMatriz();
+		int [][] Filial1 = lib.VendasFilial1();
+		int [][] Filia2 = lib.VendasFilial2();
 
 		for (int clientes = 0; clientes < 1000; clientes++) {
-			System.out.println(lib.FilialRandom());
-			// System.out.println(lib.RandomLancheQuantidade());
-			System.out.println(Arrays.toString(lib.Pedido()));
+			int [] pedidoRandom = lib.Pedido();
+			int filialRandom = lib.FilialRandom();
+			int sobrando = lib.LanchesSobrando(filialRandom,pedidoRandom);
+			System.out.println(Arrays.toString(pedidoRandom));
+			System.out.println(filialRandom);
+			System.out.println(sobrando);
 		}
 	}
 }
--- src/main/library/Library.java
+++ src/main/library/Library.java
@@ -12,18 +12,29 @@ public class Library {
 		return vendas;
 	} 
 
+	public static int[][] VendasFilial1(){
+		int[][] vendas = new int[1000][4];
+		return vendas;
+	} 
+	public static int[][] VendasFilial2(){
+		int[][] vendas = new int[1000][4];
+		return vendas;
+	} 
 
 	public static int RandomRange(int min, int max) {
 		Random rand = new Random();
 		return min + rand.nextInt((max - min) + 1);
 	}
 
+	public static int RandomLancheQuantidade() {
+		return RandomRange(1, 5); 
+	}
 	public static int[] Pedido() {
 		int[]  pedido = new int [4];
 		int max = 5;
 		for (int i = 0 ; i < pedido.length; i ++){
-			int qtde = RandomRange(1,max);
-			if ( max > 0  ){
+			int qtde = RandomLancheQuantidade();
+			if ( max > 0  && max - qtde > 0){
 				pedido[i] = qtde;
 				max = max - qtde;
 			} else {
@@ -32,7 +43,21 @@ public class Library {
 		}
 		return pedido;  
 	}
-
+	public static int LanchesSobrando(int unidade , int[] pedido){
+		int cont = 0;
+		for(int i = 0; i < pedido.length ; i ++){
+			cont = cont + pedido[i];
+		}
+		switch (unidade){
+		case 1:
+			return 200 - cont;
+		case 2:
+			return 100 -cont;
+		case 3:
+			return 50 -cont;
+		}
+		return 0;
+	}
 	public static int FilialRandom() {
 		return RandomRange(1, 3); 
 	}
