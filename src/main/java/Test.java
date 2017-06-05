7af559d0f1e73d8b3eb77199d55f5d699601c6bb
cescoferraro <francescoaferraro@gmail.com>
2017-06-05 13:13:54 -0300

    2017-06-05 13:13

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,5 @@
 package main.java;
 
-
 import java.util.Arrays;
 
 import main.library.Library;
@@ -8,18 +7,35 @@ import main.library.Library;
 public class Test {
 	public static void main(String... args) {
 		Library lib = new Library();
-		// Math Numbers tests
-		int [][] Matrix = lib.VendasMatriz();
-		int [][] Filial1 = lib.VendasFilial1();
-		int [][] Filia2 = lib.VendasFilial2();
-
-		for (int clientes = 0; clientes < 1000; clientes++) {
-			int [] pedidoRandom = lib.Pedido();
-			int filialRandom = lib.FilialRandom();
-			int sobrando = lib.LanchesSobrando(filialRandom,pedidoRandom);
-			System.out.println(Arrays.toString(pedidoRandom));
-			System.out.println(filialRandom);
-			System.out.println(sobrando);
-		}
+		lib.populate(700);
+		int[] matrizResultados = lib.totlaDeVendas(1);
+		int[] filial1Resultados = lib.totlaDeVendas(2);
+		int[] filial2Resultados = lib.totlaDeVendas(3);
+		System.out.println(Arrays.toString(matrizResultados));
+		System.out.println(Arrays.toString(filial1Resultados));
+		System.out.println(Arrays.toString(filial2Resultados));
+		// Primeira
+		System.out.println(lib.TOTALMAX());
+		//Segunda
+		System.out.println(lib.LancheMaisVendido());
+		// Terceira
+		System.out.println(lib.LancheMenosVendido());
+		// Quarta
+		int ganahdor = lib.arecadadoPorLanche(lib.LancheMaisVendido());
+		int perdedor =lib.arecadadoPorLanche(lib.LancheMenosVendido());
+		System.out.println(ganahdor - perdedor);
+		// Quinta
+		System.out.println(Arrays.toString( lib.MaiorPedido(lib.matrix) ));
+		System.out.println(Arrays.toString( lib.MaiorPedido(lib.filial1) ));
+		System.out.println(Arrays.toString( lib.MaiorPedido(lib.filial2) ));
+		int[] maiorPedidoMatriz =  lib.MaiorPedido(lib.matrix);
+		int[] maiorPedidoFilial1 = lib.MaiorPedido(lib.filial1 );
+		int [] maiorPedidoFilial2 = lib.MaiorPedido(lib.filial2 );
+		//Sexta
+		// Varios pedidos chegaram no limite máximo de lanches por pedido.
+		// fica impossível de dizer qual a venda com mais lanches.
+		// Na quinta questão eu to retornando o último pedido com o número máximo
+		// pq acontece a mesma coisa em cada unidade.
+		// System.out.println(lib.LancheMaisVendido());
 	}
 }
--- src/main/library/Library.java
+++ src/main/library/Library.java
@@ -1,64 +1,250 @@
 package main.library;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Library {
+	public static int[][] matrix = new int[1][4];
+	public static int[][] filial1 = new int[1][4];
+	public static int[][] filial2 = new int[1][4];
+	public static int[] precos = { 15, 28, 30, 25 };
+
 	public static void main(String... args) {
 		System.out.println("Library is instanciated!");
 	}
 
-	public static int[][] VendasMatriz(){
-		int[][] vendas = new int[1000][4];
-		return vendas;
-	} 
+	public static int VendaPorLanche(int lanche, int[][] planilhaVendas) {
+		int disponivel = 0;
+		for (int i = 0; i < planilhaVendas.length; i++) {
+			disponivel = disponivel + planilhaVendas[i][lanche];
+		}
+		return disponivel;
+	}
 
-	public static int[][] VendasFilial1(){
-		int[][] vendas = new int[1000][4];
-		return vendas;
-	} 
-	public static int[][] VendasFilial2(){
-		int[][] vendas = new int[1000][4];
-		return vendas;
-	} 
+	public static int[][] relatorioGeral() {
+		int[][] results = new int[3][4];
+		results[0] = totlaDeVendas(1);
+		results[1] = totlaDeVendas(2);
+		results[2] = totlaDeVendas(3);
+		return results;
+	}
+
+	public static int[] relatorioLanchesAcumulado() {
+		int[][] relatorio = relatorioGeral();
+		int[] results = new int[4];
+		results[0] = relatorio[0][0] + relatorio[1][0] + relatorio[2][0];
+		results[1] = relatorio[0][1] + relatorio[1][1] + relatorio[2][1];
+		results[2] = relatorio[0][2] + relatorio[1][2] + relatorio[2][2];
+		results[3] = relatorio[0][3] + relatorio[1][3] + relatorio[2][3];
+		return results;
+	}
+
+	public static int LancheMenosVendido() {
+		int[] Acumulado = relatorioLanchesAcumulado();
+		int min = Math.min(Acumulado[0], Math.min(Acumulado[1], Math.min(Acumulado[2], Acumulado[3])));
+		if (min == Acumulado[0])
+			return 1;
+		if (min == Acumulado[1])
+			return 2;
+		if (min == Acumulado[2])
+			return 3;
+		if (min == Acumulado[3])
+			return 4;
+		return 0;
+	}
+
+	public static int TamanhoPedido(int[] pedido) {
+		int cont = 0;
+		for (int i = 0; i < pedido.length; i++) {
+			cont = cont + pedido[i];
+		}
+		return cont;
+	}
+
+	public static int[] MaiorPedido(int[][] filial) {
+		int max = 0;
+		for (int i = 0; i < filial.length; i++) {
+			int cont = 0;
+			for (int j = 0; j < filial[i].length; j++) {
+				cont = cont + filial[i][j];
+			}
+			if (cont >= max)
+				max = cont;
+		}
+		int[] hey = new int[4];
+		for (int i = 0; i < filial.length; i++) {
+			int cont = 0;
+			for (int j = 0; j < filial[i].length; j++) {
+				cont = cont + filial[i][j];
+			}
+			// System.out.println(Arrays.toString(filial[i]));
+			// System.out.println(cont);
+			if (cont == max)
+				hey = filial[i];
+		}
+		return hey;
+
+	}
+
+	public static int LancheMaisVendido() {
+		int[] Acumulado = relatorioLanchesAcumulado();
+		int min = Math.max(Acumulado[0], Math.max(Acumulado[1], Math.max(Acumulado[2], Acumulado[3])));
+		if (min == Acumulado[0])
+			return 1;
+		if (min == Acumulado[1])
+			return 2;
+		if (min == Acumulado[2])
+			return 3;
+		if (min == Acumulado[3])
+			return 4;
+		return 0;
+	}
+
+	public static int arecadadoPorLanche(int lanche) {
+		int[] Acumulado = relatorioLanchesAcumulado();
+		int min = Math.max(Acumulado[0], Math.max(Acumulado[1], Math.max(Acumulado[2], Acumulado[3])));
+		switch (lanche) {
+		case 1:
+			return Acumulado[0] * precos[0];
+		case 2:
+			return Acumulado[1] * precos[1];
+		case 3:
+			return Acumulado[2] * precos[2];
+		case 4:
+			return Acumulado[3] * precos[3];
+		default:
+			return 0;
+		}
+	}
+
+	public int TOTALMAX() {
+		int cont = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				cont = cont + matrix[i][j];
+			}
+		}
+		for (int i = 0; i < filial1.length; i++) {
+			for (int j = 0; j < filial1[i].length; j++) {
+				cont = cont + filial1[i][j];
+			}
+		}
+		for (int i = 0; i < filial2.length; i++) {
+			for (int j = 0; j < filial2[i].length; j++) {
+				cont = cont + filial2[i][j];
+			}
+		}
+		return cont;
+	}
+
+	public static int[] totlaDeVendas(int unidade) {
+		int[] lanches = new int[4];
+		switch (unidade) {
+		case 1:
+			for (int i = 0; i < 4; i++) {
+				lanches[i] = VendaPorLanche(i, matrix);
+			}
+			return lanches;
+		case 2:
+			for (int i = 0; i < 4; i++) {
+				lanches[i] = VendaPorLanche(i, filial1);
+			}
+			return lanches;
+		case 3:
+			for (int i = 0; i < 4; i++) {
+				lanches[i] = VendaPorLanche(i, filial2);
+			}
+			return lanches;
+		default:
+			return lanches;
+		}
+	}
+
+	public void populate(int total) {
+		for (int clientes = 1; clientes < total; clientes++) {
+			int filial = RandomRange(1, 3);
+			int filialDisponivel = disponiveisFilial(filial);
+			int m = disponiveisFilial(1);
+			int mm = disponiveisFilial(2);
+			int mmm = disponiveisFilial(3);
+			int[] pedido = randomPedido();
+			int tamanhoPedido = TamanhoPedido(pedido);
+			// System.out.printf("%s %s %s\r\n", m, mm, mmm);
+			if (tamanhoPedido <= filialDisponivel) {
+				insertPedido(filial, pedido);
+			}
+		}
+
+	}
 
 	public static int RandomRange(int min, int max) {
 		Random rand = new Random();
 		return min + rand.nextInt((max - min) + 1);
 	}
 
-	public static int RandomLancheQuantidade() {
-		return RandomRange(1, 5); 
-	}
-	public static int[] Pedido() {
-		int[]  pedido = new int [4];
-		int max = 5;
-		for (int i = 0 ; i < pedido.length; i ++){
-			int qtde = RandomLancheQuantidade();
-			if ( max > 0  && max - qtde > 0){
-				pedido[i] = qtde;
-				max = max - qtde;
+	public int[] randomPedido() {
+		int[] pedido = new int[4];
+		int qtde = RandomRange(1, 5);
+		for (int i = 0; i < pedido.length; i++) {
+			if (qtde > 0) {
+				int randomQtde = RandomRange(1, qtde);
+				pedido[i] = randomQtde;
+				qtde = qtde - randomQtde;
 			} else {
-				pedido[i]=  0;
+				pedido[i] = 0;
 			}
 		}
-		return pedido;  
+		return pedido;
 	}
-	public static int LanchesSobrando(int unidade , int[] pedido){
-		int cont = 0;
-		for(int i = 0; i < pedido.length ; i ++){
-			cont = cont + pedido[i];
+
+	public static int VendasAGORA(int[][] planilhaVendas) {
+		int disponivel = 0;
+		for (int i = 0; i < planilhaVendas.length; i++) {
+			for (int j = 0; j < planilhaVendas[i].length; j++) {
+				disponivel = disponivel + planilhaVendas[i][j];
+			}
 		}
-		switch (unidade){
+		return disponivel;
+	}
+
+	public int disponiveisFilial(int unidade) {
+		switch (unidade) {
 		case 1:
-			return 200 - cont;
+			return 200 - VendasAGORA(matrix);
 		case 2:
-			return 100 -cont;
+			return 100 - VendasAGORA(filial1);
 		case 3:
-			return 50 -cont;
+			return 50 - VendasAGORA(filial2);
+		default:
+			return 0;
 		}
-		return 0;
 	}
-	public static int FilialRandom() {
-		return RandomRange(1, 3); 
+
+	private static int[][] push(int[][] array, int[] push) {
+		int[][] longer = new int[array.length + 1][];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				longer[i] = array[i];
+			}
+		}
+		longer[array.length] = push;
+		return longer;
 	}
+
+	public void insertPedido(int unidade, int[] pedido) {
+		switch (unidade) {
+		case 1:
+			matrix = push(matrix, pedido);
+			return;
+		case 2:
+			filial1 = push(filial1, pedido);
+			return;
+		case 3:
+			filial2 = push(filial2, pedido);
+			return;
+		default:
+			return;
+		}
+	}
+
 }
